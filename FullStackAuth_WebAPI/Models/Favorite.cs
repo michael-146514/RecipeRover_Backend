1cc6aef661bc38cfbc3c4a1471b8db8f890d5e17
Michael Santamaria <38706038+michael-146514@users.noreply.github.com>
2023-11-20 17:45:54 -0600

    Updated Favorite Model, Fixed userId

--- FullStackAuth_WebAPI/Models/Favorite.cs
+++ FullStackAuth_WebAPI/Models/Favorite.cs
@@ -5,7 +5,6 @@ namespace FullStackAuth_WebAPI.Models
     public class Favorite
     {
         public int FavoriteId { get; set; }
-        public int UserId { get; set; }
         public int RecipeId { get; set; }
         [ForeignKey("User")]
         public string UserId { get; set; }
