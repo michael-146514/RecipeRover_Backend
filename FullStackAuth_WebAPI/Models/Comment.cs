0128f237b6b56aa250d7165af8c79b6276b97513
Michael Santamaria <38706038+michael-146514@users.noreply.github.com>
2023-11-20 17:53:55 -0600

    Added Comment Model

---
+++ FullStackAuth_WebAPI/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FullStackAuth_WebAPI.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        public int Rating { get; set; }
+        public int RecipeId { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
