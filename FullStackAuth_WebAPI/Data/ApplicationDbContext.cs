d40235e1dfe748ede3661ddfefd2fc6b006e56c5
Michael Santamaria <38706038+michael-146514@users.noreply.github.com>
2023-11-20 17:23:44 -0600

    Added recipe Model

--- FullStackAuth_WebAPI/Data/ApplicationDbContext.cs
+++ FullStackAuth_WebAPI/Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ namespace FullStackAuth_WebAPI.Data
 {
     public class ApplicationDbContext : IdentityDbContext<User>
     {
-        public DbSet<Car> Cars { get; set; }
+        
 
         public ApplicationDbContext(DbContextOptions options)
     : base(options)
--- FullStackAuth_WebAPI/Models/Recipe.cs
+++ FullStackAuth_WebAPI/Models/Recipe.cs
@@ -1,6 +1,14 @@
-﻿namespace FullStackAuth_WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FullStackAuth_WebAPI.Models
 {
     public class Recipe
     {
+        public int RecipeId { get; set; }
+        public string Title { get; set; }
+        public string Ingredients { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public User User { get; set; }
     }
 }
