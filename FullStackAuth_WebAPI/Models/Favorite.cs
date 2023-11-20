c4c0953248681450367acb33b3e4ccc6d8f37a82
Michael Santamaria <38706038+michael-146514@users.noreply.github.com>
2023-11-20 17:41:23 -0600

    Added Favorites Model

---
+++ FullStackAuth_WebAPI/Models/Favorite.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FullStackAuth_WebAPI.Models
+{
+    public class Favorite
+    {
+        public int FavoriteId { get; set; }
+        public int UserId { get; set; }
+        public int RecipeId { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
