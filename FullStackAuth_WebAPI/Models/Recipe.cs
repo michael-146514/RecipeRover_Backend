599e1adeeca45ee26bceceab8ef214fee89cede6
Michael Santamaria <38706038+michael-146514@users.noreply.github.com>
2023-11-20 18:24:35 -0600

    Updated Recipe model

--- FullStackAuth_WebAPI/Models/Recipe.cs
+++ FullStackAuth_WebAPI/Models/Recipe.cs
@@ -7,6 +7,15 @@ namespace FullStackAuth_WebAPI.Models
         public int RecipeId { get; set; }
         public string Title { get; set; }
         public string Ingredients { get; set; }
+        public string Description { get; set; }
+        public string Instruction { get; set; }
+        public string ImageUrl { get; set; }
+        public int PreparationTimeInMinutes { get; set; }
+        public int CookingTimeInMinutes { get; set; }
+        public int Servings { get; set; }
+        public string Difficulty { get; set; }
+        public string Category { get; set; }
+
         [ForeignKey("User")]
         public string UserId { get; set; }
         public User User { get; set; }
